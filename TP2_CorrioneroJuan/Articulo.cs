4503b16fdacf7aaf071274deccbce50ee5f615b0
LordJuani <juanignacio.corrionero@gmail.com>
2019-09-06 18:49:24 -0300

    3er commit agregando las clases

---
+++ TP2_CorrioneroJuan/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_CorrioneroJuan
+{
+    class Articulo
+    {
+        private
+            int id;
+        char nombre;
+        char descripcion;
+        char imagen;
+        int codigo;
+        Marca marca;
+        Categoria categoria;
+    }
+}
--- TP2_CorrioneroJuan/Form1.cs
+++ TP2_CorrioneroJuan/Form1.cs
@@ -21,21 +21,21 @@ namespace TP2_CorrioneroJuan
         private void tlSpButton_Listar_Click(object sender, EventArgs e)
         {
             frm_Listar Listar = new frm_Listar();
-           // Listar.MdiParent = this;
+            Listar.MdiParent = this;
             Listar.Show();
         }
 
         private void tlSpButton_Buscar_Click(object sender, EventArgs e)
         {
             frm_Buscar Buscar = new frm_Buscar();
-           // Buscar.MdiParent = this;
+            Buscar.MdiParent = this;
             Buscar.Show();
         }
 
         private void tlSpButton_Agregar_Click(object sender, EventArgs e)
         {
             frm_Agregar Agregar = new frm_Agregar();
-           // Agregar.MdiParent = this;
+            Agregar.MdiParent = this;
             Agregar.Show();
         }
     }
